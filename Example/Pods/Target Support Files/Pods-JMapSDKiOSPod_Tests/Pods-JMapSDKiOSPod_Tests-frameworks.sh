a0e47bc273d9a6103f03c2015efe26020913905f
Louie <lyuen@jibestream.com>
2016-06-03 13:13:00 -0400

    update

--- Example/Pods/Target Support Files/Pods-JMapSDKiOSPod_Tests/Pods-JMapSDKiOSPod_Tests-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-JMapSDKiOSPod_Tests/Pods-JMapSDKiOSPod_Tests-frameworks.sh
@@ -84,8 +84,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${PODS_ROOT}/../../JMapSDKiOSPod/Frameworks/JMap.framework"
   install_framework "$BUILT_PRODUCTS_DIR/JMapSDKiOSPod/JMapSDKiOSPod.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${PODS_ROOT}/../../JMapSDKiOSPod/Frameworks/JMap.framework"
   install_framework "$BUILT_PRODUCTS_DIR/JMapSDKiOSPod/JMapSDKiOSPod.framework"
 fi
